bd4cba14d5a1b8e0b16458e2aafef25191abc71d
grischan <grischan@gmail.com>
2025-06-17 11:13:55 +0200

    apps:augemntetreality: Added a new COmbine (Topic + Variant) ViewsSet + changed RelatedName as objects is not a good Idea

--- adhocracy-plus/config/urls.py
+++ adhocracy-plus/config/urls.py
@@ -25,6 +25,7 @@ from adhocracy4.polls.api import PollViewSet
 from adhocracy4.ratings.api import RatingViewSet
 from adhocracy4.reports.api import ReportViewSet
 from apps.account.api import AccountViewSet
+from apps.augmentedreality import urls as ar_urls
 from apps.comments import urls as arcomments_urls
 from apps.contrib import views as contrib_views
 from apps.contrib.sitemaps import static_sitemap_index
@@ -112,6 +113,7 @@ urlpatterns = [
     path("api/", include(moderation_router.urls)),
     path("api/", include(router.urls)),
     path("api/", include(arcomments_urls)),
+    path("api/", include(ar_urls)),
     re_path(r"^api/account/", AccountViewSet.as_view(), name="api-account"),
     # API JWT authentication
     re_path(r"^api/login", obtain_auth_token, name="api-login"),
---
+++ apps/augmentedreality/api.py
@@ -0,0 +1,53 @@
+from django.contrib.contenttypes.models import ContentType
+
+from adhocracy4.comments.models import Comment
+from adhocracy4.comments_async.api import CommentViewSet
+
+from .models import Scene
+from .models import Variant
+
+# from adhocracy4.comments_async.serializers import ThreadListSerializer
+# from adhocracy4.comments.serializers import ThreadSerializer
+
+
+class CombinedCommentViewSet(CommentViewSet):
+    def get_queryset(self):
+        # First get the normal comments of the topic
+        topic_comments = super().get_queryset()
+
+        if self.content_type.model == "topic":
+            # Get the scene of the topic
+            scene = Scene.objects.filter(
+                content_type_id=self.content_type_id,  # Correction here
+                object_id=self.object_pk,
+            ).first()
+
+            if scene:
+                # Get all objects of the scene
+                scene_variants = Variant.objects.filter(
+                    object__scene=scene
+                ).values_list("id", flat=True)
+                # Get the comments of the variants
+                variant_content_type = ContentType.objects.get_for_model(Variant)
+                variant_comments = (
+                    Comment.objects.filter(
+                        object_pk__in=scene_variants,
+                        content_type_id=variant_content_type.id,
+                    )
+                    .annotate_positive_rating_count()
+                    .annotate_negative_rating_count()
+                )
+
+                # For list action only top-level comments
+                if self.action == "list":
+                    child_comment_content_type_id = ContentType.objects.get_for_model(
+                        Comment
+                    )
+                    variant_comments = variant_comments.exclude(
+                        content_type_id=child_comment_content_type_id
+                    )
+
+                # Combine and sort by creation date
+                return (topic_comments | variant_comments).order_by("-created")
+
+        return topic_comments
