7093484fba523a5665c17353b896e336cb71eb46
grischan <grischan@gmail.com>
2025-06-18 22:17:02 +0200

    apps:augemntetreality: Change CombineViewsSet to include Topics Comments from Variant Comments aswell

--- apps/augmentedreality/api.py
+++ apps/augmentedreality/api.py
@@ -13,41 +13,80 @@ from .models import Variant
 class CombinedCommentViewSet(CommentViewSet):
     def get_queryset(self):
         # First get the normal comments of the topic
-        topic_comments = super().get_queryset()
-
+        comments = super().get_queryset()
         if self.content_type.model == "topic":
-            # Get the scene of the topic
-            scene = Scene.objects.filter(
-                content_type_id=self.content_type_id,  # Correction here
-                object_id=self.object_pk,
-            ).first()
+            comments = self.get_variant_comments(comments)
+        if self.content_type.model == "variant":
+            comments = self.get_topic_comments(comments)
+        return comments
 
-            if scene:
-                # Get all objects of the scene
-                scene_variants = Variant.objects.filter(
-                    object__scene=scene
-                ).values_list("id", flat=True)
-                # Get the comments of the variants
-                variant_content_type = ContentType.objects.get_for_model(Variant)
-                variant_comments = (
-                    Comment.objects.filter(
-                        object_pk__in=scene_variants,
-                        content_type_id=variant_content_type.id,
-                    )
-                    .annotate_positive_rating_count()
-                    .annotate_negative_rating_count()
+    def get_variant_comments(self, topic_comments):
+        # Get the scene of the topic
+        scene = Scene.objects.filter(
+            content_type_id=self.content_type_id,  # Correction here
+            object_id=self.object_pk,
+        ).first()
+        print(scene)
+
+        if scene:
+            # print("Scene " + str(scene) + " " + str(scene.pk))
+            # Get all objects of the scene
+            scene_variants = Variant.objects.filter(object__scene=scene).values_list(
+                "id", flat=True
+            )
+            print(scene_variants)
+            # Get the comments of the variants
+            variant_content_type = ContentType.objects.get_for_model(Variant)
+            variant_comments = (
+                Comment.objects.filter(
+                    object_pk__in=scene_variants,
+                    content_type_id=variant_content_type.id,
                 )
+                .annotate_positive_rating_count()
+                .annotate_negative_rating_count()
+            )
 
-                # For list action only top-level comments
-                if self.action == "list":
-                    child_comment_content_type_id = ContentType.objects.get_for_model(
-                        Comment
-                    )
-                    variant_comments = variant_comments.exclude(
-                        content_type_id=child_comment_content_type_id
-                    )
+            # For list action only top-level comments
+            if self.action == "list":
+                variant_comments = self.exclude_child_comments(variant_comments)
 
-                # Combine and sort by creation date
-                return (topic_comments | variant_comments).order_by("-created")
-
+            # Combine and sort by creation date
+            return (topic_comments | variant_comments).order_by("-created")
         return topic_comments
+
+    def get_topic_comments(self, variant_comments):
+        # Get the variant object
+        variant = Variant.objects.get(pk=self.object_pk)
+
+        # Get the scene through the variant's object
+        scene = variant.object.scene
+
+        if scene:
+            # Get the topic (item) from the scene
+            topic = scene.item
+
+            # Get the content type for the topic
+            topic_content_type = ContentType.objects.get_for_model(topic)
+
+            # Get the comments of the topic
+            topic_comments = (
+                Comment.objects.filter(
+                    object_pk=topic.pk,
+                    content_type_id=topic_content_type.id,
+                )
+                .annotate_positive_rating_count()
+                .annotate_negative_rating_count()
+            )
+
+            # For list action only top-level comments
+            if self.action == "list":
+                topic_comments = self.exclude_child_comments(topic_comments)
+
+            # Combine and sort by creation date
+            return (variant_comments | topic_comments).order_by("-created")
+
+        return variant_comments
+
+    def exclude_child_comments(self, comments):
+        child_comment_content_type_id = ContentType.objects.get_for_model(Comment)
+        return comments.exclude(content_type_id=child_comment_content_type_id)
