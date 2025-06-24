1ca85a525cb0f09dd7fb034125b5efb16b5d0a50
Josh <195121232+partizipation@users.noreply.github.com>
2025-06-24 17:08:39 +0200

    Add guest user created on non auth comment

--- apps/augmentedreality/api.py
+++ apps/augmentedreality/api.py
@@ -1,7 +1,21 @@
+<<<<<<< HEAD
 from adhocracy4.comments_async.api import CommentViewSet
 
+=======
+from django.contrib.contenttypes.models import ContentType
+from guest_user.mixins import AllowGuestUserMixin
 
-class CombinedCommentViewSet(CommentViewSet):
+from adhocracy4.comments.models import Comment
+from adhocracy4.comments_async.api import CommentViewSet
+
+from .models import Scene
+from .models import Variant
+
+# from adhocracy4.comments.serializers import ThreadSerializer
+
+>>>>>>> 0bd46281 (Add guest user created on non auth comment)
+
+class CombinedCommentViewSet(AllowGuestUserMixin, CommentViewSet):
     def get_queryset(self):
         comments = super().get_queryset()
         return comments
