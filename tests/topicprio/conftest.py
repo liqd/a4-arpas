024e343f86518b5a947ab6c246eba073c7113be5
Josh <195121232+partizipation@users.noreply.github.com>
2025-07-01 15:23:49 +0200

    start addings ar tests

--- tests/topicprio/conftest.py
+++ tests/topicprio/conftest.py
@@ -3,3 +3,5 @@ from pytest_factoryboy import register
 from . import factories as topicprio
 
 register(topicprio.TopicFactory)
+register(topicprio.ARObjectFactory)
+register(topicprio.VariantFactory)
