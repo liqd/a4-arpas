b3f1c4a93d5c7ecb92ac94d42d8a0cdf57269d9e
Josh <195121232+partizipation@users.noreply.github.com>
2025-07-01 11:41:14 +0200

    Add ratings url and viewset

--- apps/augmentedreality/api.py
+++ apps/augmentedreality/api.py
@@ -7,11 +7,14 @@ from guest_user.mixins import AllowGuestUserMixin
 
 from adhocracy4.comments.models import Comment
 from adhocracy4.comments_async.api import CommentViewSet
+from adhocracy4.ratings.api import RatingViewSet
 
 from .models import Scene
 from .models import Variant
 
-# from adhocracy4.comments.serializers import ThreadSerializer
+
+class CombinedRatingViewSet(AllowGuestUserMixin, RatingViewSet):
+    http_method_names = ["post", "put", "patch", "delete", "head", "options"]
 
 >>>>>>> 0bd46281 (Add guest user created on non auth comment)
 
--- apps/augmentedreality/urls.py
+++ apps/augmentedreality/urls.py
@@ -1,8 +1,10 @@
 from adhocracy4.api import routers as a4routers
 
 from .api import CombinedCommentViewSet
+from .api import CombinedRatingViewSet
 
 ct_router = a4routers.ContentTypeDefaultRouter()
 ct_router.register(r"arpas_comments", CombinedCommentViewSet, basename="arpas_comments")
+ct_router.register(r"arpas_ratings", CombinedRatingViewSet, basename="arpas_ratings")
 
 urlpatterns = ct_router.urls
