539f4bd029161f16a312c3d02b6f0b397ce86255
Josh <195121232+partizipation@users.noreply.github.com>
2025-07-01 11:41:14 +0200

    Add ratings url and viewset

--- apps/augmentedreality/api.py
+++ apps/augmentedreality/api.py
@@ -3,11 +3,14 @@ from guest_user.mixins import AllowGuestUserMixin
 
 from adhocracy4.comments.models import Comment
 from adhocracy4.comments_async.api import CommentViewSet
+from adhocracy4.ratings.api import RatingViewSet
 
 from .models import Scene
 from .models import Variant
 
-# from adhocracy4.comments.serializers import ThreadSerializer
+
+class CombinedRatingViewSet(AllowGuestUserMixin, RatingViewSet):
+    http_method_names = ["post", "put", "patch", "delete", "head", "options"]
 
 
 class CombinedCommentViewSet(AllowGuestUserMixin, CommentViewSet):
