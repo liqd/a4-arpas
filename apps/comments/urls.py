6b876cb2e4513a64b95239102a71a023adba804b
Josh <195121232+partizipation@users.noreply.github.com>
2025-07-02 17:02:35 +0200

    Remove comments app

--- apps/comments/urls.py
+++
@@ -1,8 +0,0 @@
-from adhocracy4.api import routers as a4routers
-
-from .api import ARCommentViewSet
-
-ct_router = a4routers.ContentTypeDefaultRouter()
-ct_router.register(r"ar_comments", ARCommentViewSet, basename="comments")
-
-urlpatterns = ct_router.urls
