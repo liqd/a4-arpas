714d9b657903829588d6e108c5927b91f2dd7dd3
Shane <s.obrien@liqd.net>
2025-07-17 11:01:54 +0200

    apps/augmentedreality - improve minio config, use allowed_buckets for configuration

--- util/minio_client.py
+++ util/minio_client.py
@@ -13,7 +13,7 @@ class MinIOClient:
     def client(self):
         if all(
             attr in settings.MINIO_DATA
-            for attr in ["endpoint", "accessKey", "secretKey"]
+            for attr in ["endpoint", "accessKey", "secretKey", "allowed_buckets"]
         ):
             return Minio(
                 settings.MINIO_DATA.get("endpoint"),
@@ -30,10 +30,16 @@ class MinIOClient:
     def get_presigned_url(self, mesh_id, expires=timedelta(hours=1)):
         try:
             parts = mesh_id.split("/", 1)
-            object_name = parts[1]
-            return self.client.presigned_get_object(
-                settings.MINIO_DATA.get("bucket"), object_name, expires=expires
-            )
+            bucket_name = parts[0]
+            allowed_buckets = settings.MINIO_DATA.get("allowed_buckets")
+            if bucket_name in allowed_buckets:
+                object_name = parts[1]
+                return self.client.presigned_get_object(
+                    bucket_name, object_name, expires=expires
+                )
+            else:
+                logger.error(f"Access to bucket {bucket_name} is not allowed.")
+                return None
         except Exception as e:
             logger.error(f"Failed to generate presigned URL for {mesh_id}: {str(e)}")
             return None
